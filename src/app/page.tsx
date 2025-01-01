fec2ae407a4194cfaaa5d5baf8715afd26082c44
naoko-asano <asano@wyrd.co.jp>
2025-01-01 23:18:37 +0900

    chore: vitestの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>Diary</div>;
+  return (
+    <div className={styles.page}>
+      Diary
+      <Button />
+    </div>
+  );
 }

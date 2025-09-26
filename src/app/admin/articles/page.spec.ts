248e022081a910fb89bf1f9a8048cd148c759348
naoko-asano <asano@wyrd.co.jp>
2025-09-26 22:53:36 +0900

    test: テスト追加

---
+++ src/app/admin/articles/page.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/admin/articles");
+});
+
+test("記事タイトル一覧が表示されている", async ({ page }) => {
+  await expect(page.getByText("title1")).toBeVisible();
+  await expect(page.getByText("title2")).toBeVisible();
+});
+
+test("削除ボタンをクリックすると記事が削除される", async ({ page }) => {
+  const deletingButtons = page.getByRole("button", {
+    name: "Delete",
+  });
+
+  const firstDeleteButton = deletingButtons.first();
+
+  await expect(deletingButtons).toHaveCount(2);
+
+  await firstDeleteButton.click();
+
+  await expect(
+    page.getByText("記事を削除します。よろしいですか？"),
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "Accept" }).click();
+
+  await expect(deletingButtons).toHaveCount(1);
+  await expect(page.getByText("title1")).toHaveCount(0);
+  await expect(page.getByText("title2")).toBeVisible();
+});
